e87bbe3aace81cb141a55c1a2a482d7d8b229212
yusa0730 <isizawa5226@gmail.com>
2022-07-23 12:43:20 +0900

    add: 料理一覧のAPI取得処理

--- frontend/src/containers/Foods.jsx
+++ frontend/src/containers/Foods.jsx
@@ -1,14 +1,20 @@
-import React, {Fragment} from "react";
+import React, {Fragment, useEffect} from "react";
+
+import { fetchFoods } from "../apis/foods";
 
 export const Foods = ({
   match
 }) => {
+  useEffect(() => {
+    fetchFoods(match.params.restaurantsId)
+      .then((data) =>
+        console.log(data)
+      )
+  }, [])
+
   return (
     <Fragment>
       フード一覧
-      <p>
-        restaurantsIdは {match.params.restaurantsId}です
-      </p>
     </Fragment>
   )
 }
